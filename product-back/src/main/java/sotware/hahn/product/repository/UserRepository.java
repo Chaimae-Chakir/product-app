1280582c34c7a0fd981a66449430ccb30f1675d9
Chaimae Chakir <chaimaechakir24@gmail.com>
2025-06-23 16:35:21 +0100

    refactor(repository): update UserRepository queries

--- product-back/src/main/java/sotware/hahn/product/repository/UserRepository.java
+++ product-back/src/main/java/sotware/hahn/product/repository/UserRepository.java
@@ -6,14 +6,8 @@ import sotware.hahn.product.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
-
-    Optional<User> findByUsernameOrEmail(String username, String email);
-
     Boolean existsByUsername(String username);
-
     Boolean existsByEmail(String email);
 } 
