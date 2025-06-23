2d457b3f2350a5657cafcb5cfd222dc98b3bc8b2
Chaimae Chakir <chaimaechakir24@gmail.com>
2025-06-23 16:35:09 +0100

    feat(security): enhance custom user details and service

--- product-back/src/main/java/sotware/hahn/product/security/CustomUserDetailsService.java
+++ product-back/src/main/java/sotware/hahn/product/security/CustomUserDetailsService.java
@@ -1,4 +1,23 @@
 package sotware.hahn.product.security;
 
-public class CustomUserDetailsService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sotware.hahn.product.entity.User;
+import sotware.hahn.product.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username));
+        return new CustomUserDetails(user);
+    }
 }
