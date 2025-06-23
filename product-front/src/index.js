6097391f776d032e70ac379d3b27f20d2f171ed7
Chaimae Chakir <chaimaechakir24@gmail.com>
2025-06-23 17:36:45 +0100

    refactoring

--- product-front/src/index.js
+++ product-front/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css'; // Removed unused Bootstrap
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'primereact/resources/themes/saga-blue/theme.css';  // Theme
-import 'primereact/resources/primereact.min.css';         // Core CSS
-import 'primeicons/primeicons.css';                       // Icons
-import 'primeflex/primeflex.css';                         // PrimeFlex
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,8 +18,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
